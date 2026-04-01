55119bd683318f9ff4ed707a25ffbcfdb9427d9f
leevjo <47852192+cybrsucks@users.noreply.github.com>
2026-04-01 06:38:10 +0000

    aa

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform { 
+   required_providers { 
+      aws = { 
+         source = "hashicorp/aws" 
+         version = "~> 6.0" 
+      } 
+   } 
+} 
+ 
+provider "aws" { 
+   region = "us-east-1" 
+   shared_credentials_files = ["./credentials"] 
+   default_tags { 
+      tags = { 
+         Environment = "Dev" 
+         Course = "CSSE6400" 
+         StudentID = "<Your Student ID>" 
+      } 
+ } 
+}
+       
